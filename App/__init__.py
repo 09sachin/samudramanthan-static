ebebe421aeda4bb5cc68dfb466c2db7e17fd3006
Subhankar4901 <subhankarh701@gmail.com>
2022-02-16 14:58:25 +0530

    Final commit.

--- App/__init__.py
+++ App/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 from decouple import config
 app=Flask(__name__,template_folder="templates")
 app.secret_key=config("app_secret_key")
 app.config["SQLALCHEMY_DATABASE_URI"]=config("db_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db=SQLAlchemy(app)
+csrf=CSRFProtect(app)
 from . import routes
